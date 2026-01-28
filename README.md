2c2820cbd6226f1c41f45e2cc23849352b680cb2
Shun100 <denkidenki55@gmail.com>
2026-01-29 07:00:43 +0900

    WIP

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 
 ## 実行方法
 - `C:\ForDevelop\PostgreSQL\bin\psql.exe -U postgres -f hoge.sql`
+
+## パスワード認証のスキップ方法
+- `.pgpass`を使う方法（業務向け）
+  - `pgpass.conf`を以下のパスに新規作成
+    - `C:\Users\＜Windowsユーザー名＞\AppData\Roaming\postgresql\pgpass.conf`
+  - 中身を書く
+    - `hostname:port:database:username:password`
+
+- `pg_hba.conf`を使う方法（ローカル開発限定）
+  - `pg_hba.conf`を編集
+    - `C:\Program Files\PostgreSQL\<バージョン>\data\pg_hba.conf`
+  - `IPv4 local connections`を`trust`に変更
+    - `host    all     all     127.0.0.1/32    trust`
+  - PostgreSQLを再起動
+    - `pg_ctl reload`
+
+## トラブルシューティング
+  - ターミナルの文字コード変更
+    - `chcp 65001`
--- sample.sql
+++ sample.sql
@@ -1,13 +1,40 @@
+SET client_encoding = 'UTF8';
+SHOW client_encoding;
+
 CREATE DATABASE sql_practice;
 
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name TEXT,
-  age INT
+-- テーブルの作成
+-- CREATE TABLE users (
+--   id SERIAL PRIMARY KEY,
+--   name TEXT,
+--   age INT
+-- );
+
+-- テーブルへの挿入
+-- INSERT INTO users (name, age) VALUES
+-- ('Alice', 20),
+-- ('Bob', 25);
+
+-- テーブルからの取得
+SELECT * FROM users;
+
+-- テーブルの削除（データ戻せないので注意）
+-- DROP TABLE Product;
+
+CREATE TABLE Product (
+  -- product_id CHAR(4) NOT NULL,
+  product_id INTEGER GENERATED ALWAYS AS IDENTITY, -- auto number
+  product_name VARCHAR(32) NOT NULL,
+  product_category VARCHAR(12) NOT NULL,
+  created_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (product_id)
 );
 
-INSERT INTO users (name, age) VALUES
-('Alice', 20),
-('Bob', 25);
+INSERT INTO Product (product_name, product_category) VALUES
+  ('ゼロから作るDeep Learning', 'book');
 
-SELECT * FROM users;
+SELECT * FROM Product;
+
+-- DATE: 日付だけ 現在時刻を入れるときは`CURRENT_DATE`
+-- TIMESTAMP: 日付+時刻 現在日時を入れるときは`CURRENT_TIMESTAMP`
